4bee882431900a893e13ab127fc9c1706525d691
Rao Hamd Ullah <rao.hamd.creator@gmail.com>
2026-02-12 00:22:29 +0500

    Update stripe.js

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -1,9 +1,9 @@
-// TODO: Replace with your actual Stripe Payment Link
-// Create at: https://dashboard.stripe.com/payment-links
-// Set success_url to: https://will-you-be-my-valentine-97uc.vercel.app/valentine
+// Payment functionality disabled for demo
+// The app now redirects directly to the valentine page
 
-export const STRIPE_PAYMENT_LINK = 'https://buy.stripe.com/test_XXXXXXXXXXXXX';
+export const STRIPE_PAYMENT_LINK = ''; // Not used in demo mode
 
 export const redirectToPayment = () => {
-  window.location.href = STRIPE_PAYMENT_LINK;
+  // Redirect to valentine page instead of payment
+  window.location.href = '/valentine';
 };
